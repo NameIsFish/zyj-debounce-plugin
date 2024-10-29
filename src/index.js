d063d76b615dc0d046230c05fe8e397268fae1b7
zhangyj <zhangyj@gitlab.com>
2024-10-29 15:15:55 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
-import { createApp } from "vue";
-
 function zyjDebounce(fn, wait, immediate) {
   let timeout;
   return function (...args) {
     const context = this;
     const later = function () {
       timeout = null;
-      if (!immediate) {
-        fn.apply(context, args);
-      }
+      if (!immediate) fn.apply(context, args);
       const callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
-      if (callNow) {
-        fn.apply(context, args);
-      }
+      if (callNow) fn.apply(context, args);
     };
   };
 }
 
 export default {
-  install(app, options) {
+  install(app) {
     app.config.globalProperties.$zyjDebounce = zyjDebounce;
   }
 };
